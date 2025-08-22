c995990118b7fe33206237213a953870fd4dd2bc
João Nazário <72446679+joaonazario@users.noreply.github.com>
2025-08-22 18:49:04 -0300

    Aula 2.2 - Tipos de Dados - Exercícios

---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,13 @@
+'''
+2 - Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+Some os valores da segunda e terceira variável e armazene em outra variável.
+Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+'''
+
+text_value = "o resultado é"
+n_value = 10
+n2_value = 3.5
+result_value = n_value + n2_value
+print(text_value, n_value, n2_value, result_value)
+print(type(text_value), type(n_value), type(n2_value), type(result_value))
